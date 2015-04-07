0cc17cd5acc4705587aa0c33a95c5e6bec81fc7f
Qingqian <zhaoqingqian21@hotmail.com>
2015-04-07 02:05:58 -0400

    sorting stuff on time modified

--- public/js/private_chat.js
+++ public/js/private_chat.js
@@ -14,8 +14,6 @@ client_socket.on('connect', function(){
 	});
 });
 
-var private_history = $('#private_messages');
-
 //form self message block
 var message_self_html1 = '<li class="media"><div class="media-body">\
 							<div class="media-body">\
@@ -36,6 +34,7 @@ var message_target_html3 = '</small><br><p>';
 var message_target_html4 = '</p></div></li>';
 
 function displayPrivateMessages(source_private_messages, target_private_messages) {
+	$('#private_messages').html('');
 	var private_message_blocks = [];
 	for(var i=0;i<source_private_messages.length;i++) {
 		private_message_blocks.push(source_private_messages[i]);
@@ -43,7 +42,23 @@ function displayPrivateMessages(source_private_messages, target_private_messages
 	for(var i=0;i<target_private_messages.length;i++) {
 		private_message_blocks.push(target_private_messages[i]);
 	}
-	private_message_blocks.sort(function(a,b) {return a.post_time-b.post_time});
+	private_message_blocks.sort(function(a,b) {
+		var time1 = a.post_time.split("@");
+		var time2 = b.post_time.split("@");
+		if(time1[0].trim() < time2[0].trim()) {
+			return -1;
+		} else if(time1[0].trim() > time2[0].trim()) {
+			return 1;
+		} else {
+			if(time1[1].trim() < time2[1].trim()) {
+				return -1;
+			} else if(time1[1].trim() > time2[1].trim()) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	});
 	for(var i=0;i<private_message_blocks.length;i++) {
 		var private_message = private_message_blocks[i];
 		var message_html;
@@ -54,7 +69,7 @@ function displayPrivateMessages(source_private_messages, target_private_messages
 			message_html = message_target_html1 + target_user + message_target_html2 + private_message.post_time + message_target_html3 +
 							private_message.message_text + message_target_html4;
 		}
-		private_history.prepend(message_html);
+		$('#private_messages').append(message_html);
 	}
 	
 }
@@ -62,13 +77,13 @@ function displayPrivateMessages(source_private_messages, target_private_messages
 client_socket.on(source_user, function(message){
 	var private_message_html = message_target_html1 + target_user + message_target_html2 + message.post_time + message_target_html3 + 
 								message.message_text + message_target_html4;
-	private_history.append(private_message_html);
+	$('#private_messages').append(private_message_html);
 });
 //current user is receiving private messages from himself
 client_socket.on(source_user_self, function(message){
 	var private_message_html = message_self_html1 + source_user + message_self_html2 + message.post_time + message_self_html3 + 
 								message.message_text + message_self_html4;
-	private_history.append(private_message_html);
+	$('#private_messages').append(private_message_html);
 });
 
 $('#send_private_message_btn').click(function(){
